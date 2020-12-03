ba80ca82cc8654bc7fdb5e82b435adfe2993afe0
Taylor Collins <taylorcc33@gmail.com>
2020-12-02 19:01:59 -0700

    added functionality

--- src/index.js
+++ src/index.js
@@ -1,5 +1,91 @@
+//SAMPLE FUNCTIONS
 export const TEST_STRING = "test string";
 
 export function testFunction() {
   return TEST_STRING;
 }
+
+// EXPORT ALL AS FUNCTIONS AND KEEP THE NAMES THE SAME
+
+// ways to export
+
+// export const methodName = (param) => {
+// }
+
+// ----or---------
+
+// export methodName function(param){
+// }
+
+// ARRAY ITERATOR METHODS
+export const numberArray = [1, 2, 3, 4];
+
+// return array with double values in array
+// => [2,4,6,8];  hint use map
+export const doubleArray = (numberArray) => {
+  return numberArray.map((num) => {
+    return num * 2;
+  });
+};
+
+// // return array with all even numbers
+// => [2,4];  hint use filter
+export const evensOnlyArray = (numberArray) => {
+  return numberArray.filter((num) => {
+    return num % 2 === 0;
+  });
+};
+
+// // return array with sum of numbers
+// => 10;  hint use reduce
+export const arraySum = (numberArray) => {
+  return numberArray.reduce((total, num) => {
+    return total + num;
+  }, 0);
+};
+
+// // return true if every item greater than 0
+// => true; use every
+export const allNumbersGreaterThanZero = (numberArray) => {
+  return numberArray.every((num) => {
+    return num > 0;
+  });
+};
+
+// // return true if one or more items in array are odd
+// => true; use some
+export const someNumbersAreOdd = (numberArray) => {
+  return numberArray.some((num) => {
+    return num % 2 === 1;
+  });
+};
+
+// // return array double and even
+// => [4,8]; // use map and filter
+export const evensOnlyAndDoubleArray = (numberArray) => {
+  return numberArray
+    .filter((num) => {
+      return num % 2 === 0;
+    })
+    .map((num) => {
+      return num * 2;
+    });
+};
+
+// // find an item method return 'not found' if not found
+//=> 1
+export const findItem = (numberArray, value) => {
+  if (numberArray.includes(value)) {
+    return "good";
+  } else {
+    return "not found";
+  }
+};
+
+// // sort array desc order
+//=> [4,3,2,1] *desc*
+export const sortArray = (numberArray) => {
+  return numberArray.sort((a, b) => {
+    return b - a;
+  });
+};
--- tests/index.test.js
+++ tests/index.test.js
@@ -1,3 +1,4 @@
+// SAMPLE
 import { testFunction, TEST_STRING } from "../src";
 
 it("returns test string", () => {
@@ -5,4 +6,65 @@ it("returns test string", () => {
   const result = testFunction();
 
   expect(result).toBe(expected);
+});
+
+// HOMEWORK TESTS
+import {
+  numberArray,
+  doubleArray,
+  evensOnlyArray,
+  arraySum,
+  allNumbersGreaterThanZero,
+  someNumbersAreOdd,
+  evensOnlyAndDoubleArray,
+  findItem,
+  sortArray,
+} from "../src";
+
+describe("doubleArray test", () => {
+  test("array will double", () => {
+    expect(doubleArray(numberArray)).toEqual([2, 4, 6, 8]);
+  });
+});
+
+describe("evensOnlyArray test", () => {
+  test("array contains only even values", () => {
+    expect(evensOnlyArray(numberArray)).toEqual([2, 4]);
+  });
+});
+
+describe("arraySum test", () => {
+  test("array contains sum of array values", () => {
+    expect(arraySum(numberArray)).toEqual(10);
+  });
+});
+
+describe("allNumbersGreaterThanZero test", () => {
+  test("all values greater than zero", () => {
+    expect(allNumbersGreaterThanZero(numberArray)).toEqual(true);
+  });
+});
+
+describe("someNumbersAreOdd test", () => {
+  test("one or more value is odd", () => {
+    expect(someNumbersAreOdd(numberArray)).toEqual(true);
+  });
+});
+
+describe("evensOnlyAndDoubleArray test", () => {
+  test("only evens that are doubled", () => {
+    expect(evensOnlyAndDoubleArray(numberArray)).toEqual([4, 8]);
+  });
+});
+
+describe("findItem test", () => {
+  test("find item, else return 'not found'", () => {
+    expect(findItem(numberArray, 5)).toEqual("not found");
+  });
+});
+
+describe("sortArray test", () => {
+  test("sort array in desc order", () => {
+    expect(sortArray(numberArray)).toEqual([4, 3, 2, 1]);
+  });
 });
